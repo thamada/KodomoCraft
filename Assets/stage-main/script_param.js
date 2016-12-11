8b7d3792b811f8bfdbade92c41d43c2bab10d837
Tsuyoshi Hamada <hamada@progrape.jp>
2016-12-11 09:15:57 +0900

    add script_param.js

---
+++ Assets/stage-main/script_param.js
@@ -0,0 +1,25 @@
+﻿#pragma strict
+  
+var style :GUIStyle;
+var windowRect : Rect = Rect (20, 20, 300, 200);
+ 
+function OnGUI() {
+
+	windowRect = GUI.Window (0, windowRect, DoMyWindow, "Console");
+
+}
+
+function DoMyWindow (windowID : int) {
+
+	if(GUI.Button (Rect (10,20,100,20), "Quit")){
+//		Debug.Log("Hit!!");
+		Application.Quit();
+	}
+
+	if (Input.GetKey(KeyCode.Escape)) {
+		Application.Quit();
+	}
+
+	GUI.DragWindow (Rect (0,0, 10000, 20));
+}
+
