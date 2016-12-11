e3409bfd2feb490bec4ab0198abd9c5d33b94fa8
Tsuyoshi Hamada <hamada@progrape.jp>
2016-12-11 09:26:28 +0900

    add console window

--- Assets/stage-main/script_param.js
+++ Assets/stage-main/script_param.js
@@ -11,14 +11,22 @@ function OnGUI() {
 
 function DoMyWindow (windowID : int) {
 
-	if(GUI.Button (Rect (10,20,100,20), "Quit")){
-//		Debug.Log("Hit!!");
+
+
+	GUI.Label(Rect(10, 0,  200, 20), "Usage:", style);
+    GUI.Label(Rect(10, 20, 200, 20), "w,a,s,d: go/rot", style);
+    GUI.Label(Rect(10, 40, 200, 20), "Space: jump", style);
+    GUI.Label(Rect(10, 60, 200, 20), "Escape: exit", style);
+
+   	if(GUI.Button (Rect (10,80,100,20), "Quit")){
 		Application.Quit();
 	}
 
 	if (Input.GetKey(KeyCode.Escape)) {
 		Application.Quit();
 	}
+
+    
 
 	GUI.DragWindow (Rect (0,0, 10000, 20));
 }
