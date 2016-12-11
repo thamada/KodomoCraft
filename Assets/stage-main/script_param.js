c3f2763cae07837146e936ad99940a6403250d39
Tsuyoshi Hamada <hamada@progrape.jp>
2016-12-11 09:46:37 +0900

    add box

--- Assets/stage-main/script_param.js
+++ Assets/stage-main/script_param.js
@@ -13,12 +13,12 @@ function DoMyWindow (windowID : int) {
 
 
 
-	GUI.Label(Rect(10, 0,  200, 20), "Usage:", style);
-    GUI.Label(Rect(10, 20, 200, 20), "w,a,s,d: go/rot", style);
-    GUI.Label(Rect(10, 40, 200, 20), "Space: jump", style);
-    GUI.Label(Rect(10, 60, 200, 20), "Escape: exit", style);
+	GUI.Label(Rect(10, 20, 200, 20), "Usage...", style);
+    GUI.Label(Rect(10, 40, 200, 20), "w,a,s,d: go/rot", style);
+    GUI.Label(Rect(10, 60, 200, 20), "Space: jump", style);
+    GUI.Label(Rect(10, 80, 200, 20), "Escape: exit", style);
 
-   	if(GUI.Button (Rect (10,80,100,20), "Quit")){
+   	if(GUI.Button (Rect (10,100,100,20), "Quit")){
 		Application.Quit();
 	}
 
